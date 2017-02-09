cbe7928e591e2269b8e1e978d4221d369bc1d317
alexlbr <alex.lobera@gmail.com>
2017-02-09 16:37:25 +0000

    removed banner

--- src/components/Navigations/Menu.jsx
+++ src/components/Navigations/Menu.jsx
@@ -7,24 +7,8 @@ const Menu = ({ isOpen, pageWrapId, toggleMenu }) => (
     isOpen={isOpen}
     toggleMenu={toggleMenu}
   >
-    <h3 style={{ paddingTop: '50px' }}>
-      Next Bootcamp in London
-    </h3>
-    <a href="https://reactjs.academy/react-redux-training-london" target="_blank" rel="noopener noreferrer">
-      <img className="img-responsive" src="/public/img/offer-25-red.png" role="presentation" />
-    </a>
-    <p className="text-center">
-      <a className="text-center" href="https://reactjs.academy/react-redux-training-london" target="_blank" rel="noopener noreferrer">
-        <strong>25%</strong> discount voucher &#187;
-      </a>
-    </p>
-    <p className="text-center">
-      <a href="https://reactjs.academy/react-redux-training-london" target="_blank" rel="noopener noreferrer">
-        welovejavascriptlondon25
-      </a>
-    </p>
-    <hr />
-    <h3>Beers I like:</h3>
+
+    <h3 style={{ paddingTop: '50px' }}>Beers I like:</h3>
     <p>
       This is exercise question 5 ;)
     </p>
